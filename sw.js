596812bae3cd6f6dddbc3b7a50bc0f0d0e129850
DadleDK <hent.samsung.gear@gmail.com>
2026-09-17 14:07:13 +0200

    Deploy expanded adaptive quiz bank

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "studieoverblik-v3";
-const APP_SHELL = ["./", "./index.html", "./app.webmanifest", "./app-icon.svg", "./app-icon-192.png", "./app-icon-512.png", "./apple-touch-icon.png"];
+const CACHE_NAME = "studieoverblik-v4";
+const APP_SHELL = ["./", "./index.html", "./app.webmanifest", "./app-icon.svg", "./app-icon-192.png", "./app-icon-512.png", "./apple-touch-icon.png", "./quiz-data-eo.js", "./quiz-data-org.js", "./quiz-data-sm.js"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
