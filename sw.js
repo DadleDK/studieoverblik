83b2738fa5bd24afcfc7b4032291a5db9006f248
DadleDK <hent.samsung.gear@gmail.com>
2026-09-17 14:46:44 +0200

    Publish sourced CSR supplements and week overview

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "studieoverblik-v7";
+const CACHE_NAME = "studieoverblik-v8";
 const APP_SHELL = ["./", "./index.html", "./app.webmanifest", "./app-icon.svg", "./app-icon-192.png", "./app-icon-512.png", "./apple-touch-icon.png", "./quiz-data-eo.js", "./quiz-data-org.js", "./quiz-data-sm.js"];
 
 self.addEventListener("install", event => {
